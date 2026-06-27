ac509242b6b194f117b64c9a57bcff5569340a12
Zengxy0624 <zengxy0624@gmail.com>
2026-06-26 23:31:10 -0500

    refactor output paths in record_data.py for relative directory usage

--- record_data.py
+++ record_data.py
@@ -9,11 +9,11 @@ def exist_dir(dir):
 if __name__ == "__main__":
     camera_system = CameraSystem()
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    exist_dir(f"/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/data_collect")
+    exist_dir(f"data_collect")
     camera_system.record(
-        output_path=f"/home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/data_collect/{current_time}"
+        output_path=f"data_collect/{current_time}"
     )
     # Copy the camera calibration file to the output path
     os.system(
-        f"cp /home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/calibrate.pkl /home/hanxiao/Desktop/Research/proj-qqtt/proj-QQTT/data_collect/{current_time}"
+        f"cp calibrate.pkl data_collect/{current_time}"
     )
